9b91727130920839fbbe586c0064e3cc67b724eb
Barbara Denise <miranda.debarbi@gmail.com>
2023-05-18 11:22:32 -0300

    configuraciones para el deploy en back

--- api/index.js
+++ api/index.js
@@ -19,11 +19,12 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const port = process.env.port || 3001
+require('dotenv').config()
+
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
-  server.listen(port, () => {
-    console.log(`%s listening at ${port}`)
+conn.sync({ force: true }).then(() => {
+  server.listen(process.env.PORT, () => {
+    console.log(`%s listening at ${process.env.PORT}`)
   });
 });
